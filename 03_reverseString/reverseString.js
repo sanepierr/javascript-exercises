9429c44f885d97a92fbdc67e645212f1936c8b3d
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2025-03-07 10:34:26 +0300

    test_03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    reversedLst = [];
+    splitLst = string.trim().split(' ');
+    for (let i of splitLst){
+        prototype = [];
+        for(let k = i.length - 1, j = 0; j <= k; k--){
+            prototype.push(i[k]);
+        }
+        prototype = prototype.join('');
+        reversedLst.push(prototype);
+        
+    }
+    return String((reversedLst.reverse().join(' ')));
 };
+
+console.log(reverseString('hello there'));
 
 // Do not edit below this line
 module.exports = reverseString;
