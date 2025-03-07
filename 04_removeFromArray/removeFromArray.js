d6b91bb042674c347a5e93645f69f30b818da580
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2025-03-07 14:26:04 +0300

    test_04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...extra) {
+    arr = arr.filter(item => !extra.includes(item));
+    return arr;
 };
 
 // Do not edit below this line
