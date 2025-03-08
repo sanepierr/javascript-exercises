7f60a2d183e4b55e20cbd2470b444cf7a55ca5c5
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2025-03-08 18:05:23 +0300

    test_05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0){
+        return "ERROR";
+    }
+    if (!Number.isInteger(x) || !Number.isInteger(y)){
+        return "ERROR";
+    }
 
+    let sum = 0;
+    if (x < y){
+        for (let i = x; i <= y; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    for (let i = y; i <= x; i++){
+        sum += i;
+    }
+    return sum;
+    
+    
 };
 
 // Do not edit below this line
