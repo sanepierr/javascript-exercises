36bc487c9f583028d17ed7c01dd69c429d16a9c8
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2025-03-09 10:53:49 +0300

    test_07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp_cel = (temp - 32) * 5/9;
+  return Math.round(temp_cel * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let temp_fah = (temp * 9/5) + 32;
+  return Math.round(temp_fah  * 10) / 10;
 };
 
 // Do not edit below this line
