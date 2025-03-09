a9a014eb9c3e50d09a61bb3909cb9684230edf1a
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2025-03-09 12:59:01 +0300

    test_09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const reversed = function(string){
+        string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"");
+        let reversedLst = []
+        for (const i of string){
+            reversedLst.unshift(i);
+        }
+        let reversedString = reversedLst.join("");
+        return reversedString;
+    }
+    if (reversed(word).toLowerCase() == word.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"")){
+        
+        return true;
+    }
+    else return false;
 
 };
+console.log(palindromes("A car, a man, a maraca."));
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
